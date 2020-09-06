621d2548d9be5a90547828dd8151dc6fdd159fbd
Randel Ramirez <randelramirez1@gmail.com>
2020-09-06 15:49:37 +0800

    Implemented Custom Validation Attribute

---
+++ RestAPIConcepts/ValidationAttributes/NameMustBeDifferentWithAddressAttribute.cs
@@ -0,0 +1,27 @@
+﻿using RestAPIConcepts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPIConcepts.ValidationAttributes
+{
+    public class NameMustBeDifferentWithAddressAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value,
+          ValidationContext validationContext)
+        {
+            // if this was for a property and not a class level attribute then the value is actual value of the property where the attribute is applied to
+            var supplier = (validationContext.ObjectInstance as CreateSupplierGuidViewModel);
+
+            if (supplier.Name == supplier.Address)
+            {
+                return new ValidationResult(ErrorMessage,
+                    new[] { nameof(CreateSupplierGuidViewModel) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- RestAPIConcepts/ViewModels/CreateSupplierGuidViewModel.cs
+++ RestAPIConcepts/ViewModels/CreateSupplierGuidViewModel.cs
@@ -1,16 +1,47 @@
-﻿using System;
+﻿using RestAPIConcepts.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestAPIConcepts.ViewModels
 {
+    [NameMustBeDifferentWithAddress(ErrorMessage = "Name and Address cannot be the same")]
     public class CreateSupplierGuidViewModel
     {
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
 
+        [Required]
+        [MaxLength(200)]
         public string Address { get; set; }
 
         // PRODUCTS?
+    }
+
+    public class CreateSupplierGuidViewModelOld : IValidatableObject
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        // PRODUCTS?
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Name == Address)
+            {
+                // We don't always need to return the class name, 
+                //but since this a cross-property validation we used the actual class name instead
+                yield return new ValidationResult("The provided description should be different from the title", 
+                    new[] { "CourseForCreationDto" });
+            }
+        }
     }
 }
