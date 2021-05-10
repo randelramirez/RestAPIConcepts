07726575f4d1f0bc88d9fe867e3e603385aa17e6
Randel Ramirez <randelramirez1@gmail.com>
2021-05-10 15:32:40 +0800

    code clean up

--- RestAPIConcepts/ViewModels/CreateSupplierGuidViewModel.cs
+++ RestAPIConcepts/ViewModels/CreateSupplierGuidViewModel.cs
@@ -1,9 +1,6 @@
 ﻿using RestAPIConcepts.ValidationAttributes;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestAPIConcepts.ViewModels
 {
